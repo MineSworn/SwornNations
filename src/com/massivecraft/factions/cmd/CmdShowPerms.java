24f954c6d7f0da5c000e345b7fea5ad187d42fb2
Dan Mulloy <dmulloy2@live.com>
2013-07-01 14:37:45 -0400

    Fix error with /f showperms

--- src/com/massivecraft/factions/cmd/CmdShowPerms.java
+++ src/com/massivecraft/factions/cmd/CmdShowPerms.java
@@ -28,21 +28,29 @@ public class CmdShowPerms extends FCommand {
 	}
 	
 	@Override
-	public void perform() {		
+	public void perform() 
+	{		
 		Faction f = fme.getFaction();
 		
-		if (args.get(0).equalsIgnoreCase("p")) {
+		if (args.get(0).equalsIgnoreCase("p")) 
+		{
 			FPlayer you = this.argAsBestFPlayerMatch(1);
+			if (you == null)
+				return;
+			
 			if (!you.hasFaction() || !fme.hasFaction())
 				return;
-			if (you.getFaction() != fme.getFaction()) {
+			
+			if (you.getFaction() != fme.getFaction())
+			{
 				msg("%s<i> is not in your faction.", you.describeTo(fme));
 				return;
 			}
 			
 			msgHeader(you.describeTo(fme));
 			StringBuilder ret = new StringBuilder();
-			for (NPermission perm : NPermission.values()) {
+			for (NPermission perm : NPermission.values()) 
+			{
 				if (f.playerHasPermission(you, perm))
 					ret.append(ChatColor.GREEN + perm.toString() + ", ");
 				else
@@ -50,7 +58,9 @@ public class CmdShowPerms extends FCommand {
 			}
 			ret.deleteCharAt(ret.lastIndexOf(","));
 			msg(ret.toString());
-		} else if (args.get(0).equalsIgnoreCase("r")) {
+		}
+		else if (args.get(0).equalsIgnoreCase("r")) 
+		{
 			if (!fme.hasFaction())
 				return;
 			Role role = null;
@@ -63,7 +73,8 @@ public class CmdShowPerms extends FCommand {
 			else if (args.get(1).equalsIgnoreCase("default") || args.get(1).equalsIgnoreCase("normal"))
 				role = Role.NORMAL;
 			
-			if (role == null) {
+			if (role == null) 
+			{
 				msg("<i>%s <b>did not match any roles.", args.get(1));
 				return;
 			}
@@ -71,7 +82,8 @@ public class CmdShowPerms extends FCommand {
 			msgHeader(role.nicename);
 			
 			StringBuilder ret = new StringBuilder();
-			for (NPermission perm : NPermission.values()) {
+			for (NPermission perm : NPermission.values()) 
+			{
 				if (f.roleHasPermission(role, perm))
 					ret.append(ChatColor.GREEN + perm.toString() + ", ");
 				else
@@ -82,8 +94,8 @@ public class CmdShowPerms extends FCommand {
 		}
 	}
 	
-	public void msgHeader(String name) {
+	public void msgHeader(String name) 
+	{
 		msg("<a>Faction permissions for " + name + "<a>:");
 	}
-	
 }
