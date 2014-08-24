10ce6bc36ba87f5c7839cbc6da2de1e0cd990565
Dan Mulloy <dmulloy2@live.com>
2014-08-23 20:30:29 -0400

    Temporarily pull LWC integration, see if that helps

--- src/main/java/com/massivecraft/factions/integration/LWCFeatures.java
+++ src/main/java/com/massivecraft/factions/integration/LWCFeatures.java
@@ -1,103 +1,85 @@
 package com.massivecraft.factions.integration;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import net.dmulloy2.swornnations.SwornNations;
-
-import org.bukkit.Bukkit;
-import org.bukkit.Chunk;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
-import org.bukkit.plugin.Plugin;
-
-import com.griefcraft.lwc.LWC;
-import com.griefcraft.lwc.LWCPlugin;
-import com.griefcraft.model.Protection;
-import com.massivecraft.factions.Conf;
 import com.massivecraft.factions.FLocation;
-import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.Faction;
 
 public class LWCFeatures
 {
-	private static LWC lwc;
+//	private static LWC lwc;
 
 	public static void setup()
 	{
-		Plugin plugin = Bukkit.getPluginManager().getPlugin("LWC");
-		if (plugin == null || ! plugin.isEnabled())
-			return;
-
-		lwc = ((LWCPlugin) plugin).getLWC();
-		SwornNations.get().log(
-				"Successfully hooked into LWC!"
-						+ (Conf.lwcIntegration ? "" : " Integration is currently disabled, though (\"lwcIntegration\")."));
+//		Plugin plugin = Bukkit.getPluginManager().getPlugin("LWC");
+//		if (plugin == null || ! plugin.isEnabled())
+//			return;
+//
+//		lwc = ((LWCPlugin) plugin).getLWC();
+//		SwornNations.get().log(
+//				"Successfully hooked into LWC!"
+//						+ (Conf.lwcIntegration ? "" : " Integration is currently disabled, though (\"lwcIntegration\")."));
 	}
 
 	public static boolean isEnabled()
 	{
-		return Conf.lwcIntegration && lwc != null;
+		return false;
+//		return Conf.lwcIntegration && lwc != null;
 	}
 
 	public static void clearOtherChests(FLocation flocation, Faction faction)
 	{
-		World world = Bukkit.getWorld(flocation.getWorldName());
-		if (world == null)
-			return; // world not loaded or something? cancel out to prevent
-					// error
-
-		Location location = new Location(world, flocation.getX() * 16, 5, flocation.getZ() * 16);
-		Chunk chunk = location.getChunk();
-		BlockState[] blocks = chunk.getTileEntities();
-		List<Block> chests = new LinkedList<Block>();
-
-		for (BlockState state : blocks)
-		{
-			if (state.getType() == Material.CHEST)
-				chests.add(state.getBlock());
-		}
-
-		for (Block chest : chests)
-		{
-			Protection prot = lwc.findProtection(chest);
-			if (prot != null)
-			{
-				boolean found = false;
-				for (FPlayer fplayer : faction.getFPlayers())
-					if (fplayer.getName().equals(prot.getOwner()))
-						found = true;
-				if (! found)
-					prot.remove();
-			}
-		}
+//		World world = Bukkit.getWorld(flocation.getWorldName());
+//		if (world == null)
+//			return; // world not loaded or something? cancel out to prevent
+//					// error
+//
+//		Location location = new Location(world, flocation.getX() * 16, 5, flocation.getZ() * 16);
+//		Chunk chunk = location.getChunk();
+//		BlockState[] blocks = chunk.getTileEntities();
+//		List<Block> chests = new LinkedList<Block>();
+//
+//		for (BlockState state : blocks)
+//		{
+//			if (state.getType() == Material.CHEST)
+//				chests.add(state.getBlock());
+//		}
+//
+//		for (Block chest : chests)
+//		{
+//			Protection prot = lwc.findProtection(chest);
+//			if (prot != null)
+//			{
+//				boolean found = false;
+//				for (FPlayer fplayer : faction.getFPlayers())
+//					if (fplayer.getName().equals(prot.getOwner()))
+//						found = true;
+//				if (! found)
+//					prot.remove();
+//			}
+//		}
 	}
 
 	public static void clearAllChests(FLocation flocation)
 	{
-		World world = Bukkit.getWorld(flocation.getWorldName());
-		if (world == null)
-			return; // world not loaded or something? cancel out to prevent error
-
-		Location location = new Location(world, flocation.getX() * 16, 5, flocation.getZ() * 16);
-		Chunk chunk = location.getChunk();
-		BlockState[] blocks = chunk.getTileEntities();
-		List<Block> chests = new LinkedList<Block>();
-
-		for (BlockState state : blocks)
-		{
-			if (state.getType() == Material.CHEST)
-				chests.add(state.getBlock());
-		}
-
-		for (Block chest : chests)
-		{
-			Protection prot = lwc.findProtection(chest);
-			if (prot != null)
-				prot.remove();
-		}
+//		World world = Bukkit.getWorld(flocation.getWorldName());
+//		if (world == null)
+//			return; // world not loaded or something? cancel out to prevent error
+//
+//		Location location = new Location(world, flocation.getX() * 16, 5, flocation.getZ() * 16);
+//		Chunk chunk = location.getChunk();
+//		BlockState[] blocks = chunk.getTileEntities();
+//		List<Block> chests = new LinkedList<Block>();
+//
+//		for (BlockState state : blocks)
+//		{
+//			if (state.getType() == Material.CHEST)
+//				chests.add(state.getBlock());
+//		}
+//
+//		for (Block chest : chests)
+//		{
+//			Protection prot = lwc.findProtection(chest);
+//			if (prot != null)
+//				prot.remove();
+//		}
 	}
 }
